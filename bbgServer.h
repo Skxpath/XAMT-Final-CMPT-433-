f039b6c8a0e1727fcaea9aa3a28f2f1b81feb4e2
Tony Yan <zifeiy@sfu.ca>
2017-11-15 00:07:52 -0800

    Rebase onto Master branch

--- bbgServer.h
+++ bbgServer.h
@@ -1,11 +1,9 @@
-#ifndef BBG_SERVER_H
-#define BBG_SERVER_H
+#ifndef _BBG_SERVER_H_
+#define _BBG_SERVER_H_
 
-
-
+// Start listening to the port on a separate thread.
 void bbgServer_startListening(void);
 
 void bbgServer_cleanup(void);
-
 
 #endif
